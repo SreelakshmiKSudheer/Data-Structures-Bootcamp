c86b2ff4236b6abeeff4b21094fb45885ce12e19
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-06-22 12:49:40 +0530

    left rotation

---
+++ Competitive Coding/Arrays/Left Rotation.py
@@ -0,0 +1,21 @@
+
+def rotateLeft(d, arr):
+    """
+    Rotates the elements of the given array to the left by a specified number of positions.
+
+    Parameters:
+        d (int): The number of positions to rotate the array to the left.
+        arr (list): The list of elements to be rotated.
+
+    Returns:
+        list: A new list with the elements rotated to the left by 'd' positions.
+
+    Example:
+        >>> rotateLeft(2, [1, 2, 3, 4, 5])
+        [3, 4, 5, 1, 2]
+    """
+    # Write your code here
+    l = len(arr)
+    result = [arr[(i+d)%l] for i in range(l)]
+    # result = [arr[(i-l+d)] for i in range(l)]
+    return result
