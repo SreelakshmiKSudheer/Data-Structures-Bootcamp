401b522d7d0875ce381849e942c6a70ea113cb37
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-07-13 15:45:48 +0530

    simple methods

--- Data Structures/Day 7/Quick Sort/Quick Sort.py
+++ Data Structures/Day 7/Quick Sort/Quick Sort.py
@@ -26,3 +26,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# def quickSort(arr):
+#     if len(arr) <= 1:
+#         return arr
+    
+#     pivot = arr[0]
+#     left = [x for x in arr[1:] if x <= pivot]
+#     right = [x for x in arr[1:] if x > pivot]
+
+#     return quickSort(left) + [pivot] + quickSort(right)
