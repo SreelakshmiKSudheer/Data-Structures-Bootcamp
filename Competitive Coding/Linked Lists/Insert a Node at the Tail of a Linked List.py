a02eb4ff3d78093a2149ddd76c81b276837fc443
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-08-30 20:35:05 +0530

    insertion at the end

--- Competitive Coding/Linked Lists/Insert a Node at the Tail of a Linked List.py
+++ Competitive Coding/Linked Lists/Insert a Node at the Tail of a Linked List.py
@@ -1,12 +1,33 @@
 def insertNodeAtTail(head, data):
+    """
+    Inserts a new node with the specified data at the tail (end) of a singly linked list.
+    Args:
+        head (SinglyLinkedListNode or None): The head node of the singly linked list. Can be None if the list is empty.
+        data (any): The data value to be stored in the new node.
+    Returns:
+        SinglyLinkedListNode: The head node of the updated singly linked list.
+    Example:
+        # Assuming SinglyLinkedListNode is defined
+        head = None
+        head = insertNodeAtTail(head, 5)
+        head = insertNodeAtTail(head, 10)
+        # The list is now: 5 -> 10
+    Note:
+        This function assumes the existence of a SinglyLinkedListNode class with 'data' and 'next' attributes.
+    """
+    # Create a new node with the given data
     new_node = SinglyLinkedListNode(data)
     
+    # If the list is empty, the new node becomes the head
     if not head:
         return new_node
 
+    # Otherwise, traverse to the end of the list
     current = head
     while current.next:
         current = current.next
+    # Link the last node to the new node
     current.next = new_node
     
+    # Return the (unchanged) head of the list
     return head
