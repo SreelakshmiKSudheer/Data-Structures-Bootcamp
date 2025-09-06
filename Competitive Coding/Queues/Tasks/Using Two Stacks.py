2cd594989fe9e27de17f95ec0cd6d9b1e6bf4476
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-09-06 14:21:25 +0530

    queue using two stacks

--- Competitive Coding/Queues/Tasks/Using Two Stacks.py
+++ Competitive Coding/Queues/Tasks/Using Two Stacks.py
@@ -1,4 +1,20 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+# from collections import deque
+
+# q = int(input())
+# queue = deque()
+# for _ in range(q):
+#     query = list(map(int, input().split()))
+    
+#     if query[0] == 1:
+#         queue.append(query[1])
+#     elif query[0] == 2:
+#         queue.popleft()     
+#     elif query[0] == 3:
+#         print(queue[0])     
+
+
+'''
 q = int(input())
 queue = []
 queries = []
@@ -13,19 +29,40 @@ for query in queries:
         queue.pop(0)
     elif query[0] == 3:
         print(queue[0])
-
 '''
-from collections import deque
 
-q = int(input())
-queue = deque()
-for _ in range(q):
-    query = list(map(int, input().split()))
-    
-    if query[0] == 1:
-        queue.append(query[1])
-    elif query[0] == 2:
-        queue.popleft()     
-    elif query[0] == 3:
-        print(queue[0]) 
-'''
+
+class MyQueue:
+    """
+    Implements a queue using two stacks.
+    Methods
+    -------
+    push(x):
+        Pushes element x to the back of the queue.
+    pop():
+        Removes and returns the element from the front of the queue.
+    peek():
+        Returns the element at the front of the queue without removing it.
+    empty():
+        Returns True if the queue is empty, False otherwise.
+    """
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x):
+        self.in_stack.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.out_stack.pop()
+
+    def peek(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1]
+
+    def empty(self):
+        return not self.in_stack and not self.out_stack
+
